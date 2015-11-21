b263d4cc4a03f400db91b6ebc11715affcba9359
fujisawa-shin <fujisawa-shin@kayac.com>
2015-11-21 22:55:57 +0900

    いろいろconfigに切る

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -25,12 +25,12 @@ var SRC_JS = [ SRC, 'js' ].join('/');
 var SRC_JS_LIB = [ SRC_JS, 'lib' ].join('/');
 var SRC_JADE = [ SRC, 'jade' ].join('/');
 var SRC_JADE_HELPER = [ SRC_JADE, 'helper' ].join('/');
-var SRC_DATA = [ SRC, 'data' ].join('/');
+var SRC_CONFIG = [ SRC, 'config' ].join('/');
 
 var GLOB_SASS = path.join(SRC_SASS, '**/*.scss');
 var GLOB_JS = path.join(SRC_JS, '**/*.js');
 var GLOB_JADE = path.join(SRC_JADE, '**/*.jade');
-var GLOB_DATA = path.join(SRC_DATA, '*');
+var GLOB_CONFIG = path.join(SRC_CONFIG, '*');
 
 var DEST = '../public';
 var DEST_IMG = path.join(DEST, 'img');
@@ -45,16 +45,6 @@ var onError = notify.onError({
     title: 'Error: <%= error.plugin %> / ' + PROJ_NAME,
     message: '<%= error.message %>'
 });
-
-var loadLocals = function () {
-    var locals = util.readConfig([
-        'config/meta.yaml',
-        {
-            http_path: HTTP_PATH
-        }
-    ]);
-    return locals;
-};
 
 
 /* ========================================= *
@@ -74,25 +64,23 @@ gulp.task('css', ['sass']);
 
 // js
 gulp.task('copy-lib', function () {
-    return gulp.src([
-        'bower_components/html5shiv/dist/html5shiv.min.js'
-    ]).pipe(gulp.dest(DEST_JS_LIB));
+    var config = util.readConfig(
+        path.join(SRC_CONFIG, 'copy.yaml')
+    );
+    return gulp.src(config.js_lib).pipe(gulp.dest(DEST_JS_LIB));
 });
 
 gulp.task('compile-js', function () {
-
     gulp.src(SRC_JS + '/hinagataGulp*.js')
-        .pipe(varline({
-            wrap: true,
-            loadPath: [
-                SRC_JS + '/*.js',
-                SRC_JS_LIB + '/*.js'
-            ],
-            alias: {
-                $: 'jquery',
-                _: 'underscore'
+        .pipe(varline(util.readConfig([
+            path.join(SRC_CONFIG, 'varline.yaml'),
+            {
+                loadPath: [
+                    SRC_JS + '/*.js',
+                    SRC_JS_LIB + '/*.js'
+                ]
             }
-        }))
+        ])))
         .on('error', onError)
         .pipe(gulp.dest(DEST_JS));
 });
@@ -102,8 +90,13 @@ gulp.task('js', ['copy-lib', 'compile-js']);
 
 // html
 gulp.task('jade', function () {
-    var locals = loadLocals();
-    locals.SNSHelper = require(SRC_JADE_HELPER + '/SNSHelper');
+    var locals = util.readConfig([
+        path.join(SRC_CONFIG, 'meta.yaml'),
+        {
+            http_path: HTTP_PATH,
+            SNSHelper: require(SRC_JADE_HELPER + '/SNSHelper')
+        }
+    ]);
 
     gulp.src(SRC_JADE + '/*.jade')
         .pipe(jade({
@@ -127,7 +120,9 @@ gulp.task('server', function () {
 
 // publish
 gulp.task('publish', function () {
-    var config = util.readConfig([ 'config/aws-credentials.json' ]);
+    var config = util.readConfig([
+        path.join(SRC_CONFIG, 'aws-credentials.json')
+    ]);
     
     var publisher = awspublish.create(config);
     gulp.src(DEST + '/**/*')
@@ -160,7 +155,7 @@ gulp.task('watch', function () {
     gulp.watch(GLOB_SASS, ['sass']);
     gulp.watch(GLOB_JS, ['js']);
     gulp.watch(GLOB_JADE, ['jade']);
-    gulp.watch(GLOB_DATA, ['html']);
+    gulp.watch(GLOB_CONFIG, ['html']);
 });
 
 
